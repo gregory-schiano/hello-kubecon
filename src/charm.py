b3ed6f7e790a583640b6b7ad0d246ad3279a4b32
jdkandersson <david.andersson@canonical.com>
2023-02-28 15:54:06 +1100

    switch to replan_services

--- src/charm.py
+++ src/charm.py
@@ -28,6 +28,8 @@ from ingress import set_ as set_ingress
 from site_ import set_local_content as set_site_content
 from state import State
 
+APPLICATION_NAME = "gosherve"
+
 
 class HelloKubeconCharm(CharmBase):
     def __init__(self, *args) -> None:
@@ -35,55 +37,51 @@ class HelloKubeconCharm(CharmBase):
 
         self.framework.observe(self.on.install, self._update_site_content)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.gosherve_pebble_ready, self._update_redirect_map)
+        self.framework.observe(self.on.gosherve_pebble_ready, self._update_containers)
 
         self.framework.observe(self.on.pull_site_action, self._pull_site_action)
 
-        self.ingress = None
         self.state = State(self)
 
     def _on_config_changed(self, event=None) -> None:
         self._update_ingress(event)
-        self._update_redirect_map(event)
+        self._update_containers(event)
 
-    def _update_redirect_map(self, _event=None) -> None:
-        self.unit.status = MaintenanceStatus("Setting redirect map")
+    def _update_containers(self, _event=None) -> None:
+        self.unit.status = MaintenanceStatus("Getting application ready")
+
+        container = self.unit.get_container(APPLICATION_NAME)
+
+        if not container.can_connect():
+            self.unit.status = WaitingStatus("waiting for Pebble in workload container")
+            return
+
         gosherve_env = calculate_gosherve_env(self.state.redirect_map)
         gosherve_layer = {
-            "summary": "gosherve layer",
-            "description": "pebble config layer for gosherve",
+            "summary": f"{APPLICATION_NAME} layer",
+            "description": f"pebble config layer for {APPLICATION_NAME}",
             "services": {
-                "gosherve": {
+                APPLICATION_NAME: {
                     "override": "replace",
-                    "summary": "gosherve",
+                    "summary": APPLICATION_NAME,
                     "command": "/gosherve",
                     "startup": "enabled",
                     "environment": gosherve_env,
                 }
             },
         }
-
-        container = self.unit.get_container("gosherve")
-
-        if not container.can_connect():
-            self.unit.status = WaitingStatus("waiting for Pebble in workload container")
-        else:
-            services = container.get_plan().to_dict().get("services", {})
-            if services != gosherve_layer["services"]:
-                container.add_layer("gosherve", gosherve_layer, combine=True)
-                container.restart("gosherve")
+        container.add_layer(APPLICATION_NAME, gosherve_layer, combine=True)
+        container.pebble.replan_services()
 
         self.unit.status = ActiveStatus()
 
     def _update_ingress(self, _event=None) -> None:
-        self.unit.status = MaintenanceStatus("Updating ingress")
-        self.ingress = set_ingress(self, self.ingress, self.state.ingress)
+        self.ingress = set_ingress(
+            self, getattr(self, "ingress", None), self.state.ingress
+        )
 
     def _update_site_content(self, _event=None) -> None:
-        self.unit.status = MaintenanceStatus("Fetching web site")
         set_site_content(self.state.site_content)
-
-        self.unit.status = ActiveStatus()
 
     def _pull_site_action(self, event) -> None:
         self._update_site_content()
